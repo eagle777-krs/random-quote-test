5af4c47d9b0239b8d7db406a35b6bdeb841fa841
eagle777-krs <williamgarfield67@gmail.com>
2025-07-04 20:25:04 +0300

    wsgi settings

--- random_quote/wsgi.py
+++ random_quote/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+project_home = '/home/Lew77795/random-quote-test'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+venv_path = '/home/Lew77795/random_quote/venv'
+activate_env = os.path.join(venv_path, 'bin/activate_this.py')
+with open(activate_env) as f:
+    exec(f.read(), {'__file__': activate_env})
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_quote.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
