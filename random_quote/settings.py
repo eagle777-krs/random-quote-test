9911669f58f1d51ee60c5be96c22253f7ef3b0d8
eagle777-krs <williamgarfield67@gmail.com>
2025-07-05 12:09:23 +0300

    settings-staticdirs

--- random_quote/settings.py
+++ random_quote/settings.py
@@ -112,13 +112,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
